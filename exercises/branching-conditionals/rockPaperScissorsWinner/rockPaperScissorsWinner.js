b8927265ee48675c92940aad9e3cb4598968dbf9
Mary Grace Bunch <marygrace@Marys-MacBook-Pro-2.local>
2020-06-16 16:30:13 -0400

    responses to conditionals

--- exercises/branching-conditionals/rockPaperScissorsWinner/rockPaperScissorsWinner.js
+++ exercises/branching-conditionals/rockPaperScissorsWinner/rockPaperScissorsWinner.js
@@ -24,8 +24,36 @@
  * @returns {string} Returns 'win', 'lose', or 'draw' depending on
  *  whether the first player won, lost, or drew the match.
  */
-function rockPaperScissorsWinner(array) {
+function rockPaperScissorsWinner(firstPlayerMove, secondPlayerMove) {
   // This is your job. :)
+  if ((!Number.isInteger(firstPlayerMove) || (firstPlayerMove > 2 || firstPlayerMove < 0)) || (!Number.isInteger(secondPlayerMove) || (secondPlayerMove > 2 || secondPlayerMove < 0))){
+    throw new Error(`Expected a firstPlayerMove and secondPlayerMove between 0 and 2, but received: ${firstPlayerMove} ${secondPlayerMove}`);
+  }
+
+  if (firstPlayerMove == secondPlayerMove){
+    return 'draw';
+  } 
+
+  if (firstPlayerMove == 0){
+    if (secondPlayerMove == 1){
+      return 'win';
+    }
+    return 'lose';
+  }
+
+  if (firstPlayerMove == 1){
+    if (secondPlayerMove == 2){
+      return 'win';
+    }
+    return 'lose';
+  }
+
+  if (firstPlayerMove == 2){
+    if (secondPlayerMove == 0){
+      return 'win';
+    }
+    return 'lose';
+  }
 }
 
 if (require.main === module) {
@@ -33,6 +61,12 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+  console.log(rockPaperScissorsWinner(0, 1)); // => 'win' (scissors beats paper)
+  console.log(rockPaperScissorsWinner(1, 0)); // => 'lose' (paper loses to scissors)
+  console.log(rockPaperScissorsWinner(1, 2)); // => 'win' (paper beats rock)
+  console.log(rockPaperScissorsWinner(2, 2)); // => 'draw' (both players played rock)
+  // console.log(rockPaperScissorsWinner(4, 7)); // => throw error
+  // console.log(rockPaperScissorsWinner('rock', 2)); // => throw error
 }
 
 module.exports = rockPaperScissorsWinner;
